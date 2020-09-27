dd2179c890ee48a79a7c40bf99470284f5d87659
LisbethPinales <lisbethpinales05@gmail.com>
2020-09-26 17:07:45 -0700

    update

---
+++ practicaV.py
@@ -0,0 +1,14 @@
+# #Usando lo aprendido hasta ahora:
+# Hacer un programa con acceso a base de datos
+# que sirva como una agenda telefónica.
+# Debe tener un menu que permita:
+# 1 – agregar nuevo contacto
+# 2 – Listar todos los contactos
+# 3 – Buscar contactos por nombre o numero de teléfono
+# 4 – Actualizar un contacto
+# 5 – Eliminar un contacto
+# 6 – Salir
+# Al agregar un nuevo contacto, debe validar que el contacto no exista previamente. Si existe debe dar un
+# mensaje notificando al usuario.
+# Al terminar de agregar un contacto debe preguntar si desea agregar otro.
+
